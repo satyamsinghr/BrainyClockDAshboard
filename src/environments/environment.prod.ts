c5cf71529f33ece52c9f268c7049ab26fa0a59fc
ashutosh <ashutoshmalik1994@gmail.com>
2024-04-24 18:09:56 +0530

    commit change in base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  base_url:"http://54.210.1.150:3000/api",
+  // base_url:"http://54.210.1.150:3000/api",
+  base_url:'https://8m02o9way6.execute-api.us-east-1.amazonaws.com/prod/api',
   local_url : 'http://localhost:3000/api'
 };
