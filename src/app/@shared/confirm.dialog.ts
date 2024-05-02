f45253c0ce462dd83a68439bb5730cf034db392a
ashutosh <ashutoshmalik1994@gmail.com>
2024-05-02 10:04:36 +0530

    commit changes in profile page and setting page

--- src/app/@shared/confirm.dialog.ts
+++ src/app/@shared/confirm.dialog.ts
@@ -4,12 +4,12 @@ import { Component, OnInit, Inject } from '@angular/core';
 @Component({
   selector: 'app-confirm-dialog',
   template: `
-    <h1 mat-dialog-title style="margin-bottom:0" cdkFocusRegionStart>
-      {{ title }}
-    </h1>
+      <div class="brand_info">
+        <h2 class="text-center">{{ title }}</h2>
+    </div>
 
-    <div mat-dialog-content>
-      <p [innerHTML]="message"></p>
+    <div mat-dialog-content class="text-center">
+      <p [innerHTML]="message" class="text-center mb-0"></p>
     </div>
 
     <div mat-dialog-actions class="d-flex justify-content-center">
@@ -17,8 +17,17 @@ import { Component, OnInit, Inject } from '@angular/core';
         <button mat-raised-button color="primary" (click)="onConfirm()">OK</button>
       </div>
       <ng-template #elseBlock>
-        <button mat-raised-button color="primary" [style.background-color]="'#FF6528'" (click)="onConfirm()">Yes</button>
-        <button mat-raised-button (click)="onDismiss()">No</button>
+        <!-- <button mat-raised-button class="bnt btn-outline-primary" color="primary" [style.background-color]="'#FF6528'" (click)="onConfirm()">Yes</button>
+        <button mat-raised-button class="bnt btn-primary" (click)="onDismiss()">No</button> -->
+        <div class="header_add_button d-flex align-items-center gap-3">
+            <div class="position-relative">
+                <button class="btn btn-primary d-flex align-items-center gap-2 justify-content-center"
+                    (click)="onConfirm()">
+                    Yes
+                </button>
+            </div>
+            <button class="btn btn-outline-primary" (click)="onDismiss()">No</button>
+        </div>
       </ng-template>
     </div>
   `,
@@ -26,7 +35,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 export class ConfirmDialogComponent implements OnInit {
   title: string = 'Are you sure you want to do this?';
   message: string = 'Confirm Action';
-  status:boolean=false;
+  status: boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<ConfirmDialogComponent>,
@@ -35,10 +44,10 @@ export class ConfirmDialogComponent implements OnInit {
     // Update view with given values
     this.title = data.title;
     this.message = data.message;
-    this.status= data.status;
+    this.status = data.status;
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onConfirm(): void {
     // Close the dialog, return true
@@ -56,6 +65,6 @@ export class ConfirmDialogComponent implements OnInit {
  * It has been kept here to keep it as part of shared component.
  */
 export class ConfirmDialogModel {
-  constructor(public title: string, public message: any, public status:boolean) {}
+  constructor(public title: string, public message: any, public status: boolean) { }
 }
 
